9664951acbd2f731d89ce783913b644c4941157e
Telesphore8527 <telesphorekuedjeu@gmail.com>
2026-07-29 10:53:44 +0100

    mise au point du temps et affichage correct du temps dans sa zone

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ function App() {
   const [heartNumber, setHeartNumber] = useState(3);
   const [time, setTime] = useState(10);
 
+    /*======================================================
+      ----------------Online Mode-----------------------=
+      ====================================================== */
   useEffect(() => {
     const call = async () => {
       let result = await fetch(
@@ -25,11 +28,55 @@ function App() {
     call();
   }, []);
 
-  useEffect(() => {
-    let max = Math.max(score, hightScore);
-    setHightScore(max);
-    localStorage.setItem("hightScore", JSON.stringify(max));
-  }, [score]);
+  /*======================================================
+      ----------------Online Mode-----------------------=
+      ====================================================== */
+
+//   useEffect(() => {
+//     const call =  () => {
+//       let result = `{
+//   "response_code": 0,
+//   "results": [
+//     {
+//       "question": "Le film 'Star Wars' a été réalisé par George Lucas.",
+//       "correct_answer": "True",
+//       "incorrect_answers": ["False"]
+//     },
+//     {
+//       "question": "Les personnages de 'Harry Potter' sont tous réels.",
+//       "correct_answer": "False",
+//       "incorrect_answers": ["True"]
+//     },
+//     {
+//       "question": "Le film 'Jurassic Park' est basé sur un roman de Stephen King.",
+//       "correct_answer": "False",
+//       "incorrect_answers": ["True"]
+//     },
+//     {
+//       "question": "La série 'Stranger Things' est une production américaine.",
+//       "correct_answer": "True",
+//       "incorrect_answers": ["False"]
+//     },
+//     {
+//       "question": "Le film 'The Godfather' a été réalisé par Francis Ford Coppola.",
+//       "correct_answer": "True",
+//       "incorrect_answers": ["False"]
+//     }
+//   ]
+// }
+// `
+//       const data = JSON.parse(result);
+//       setQuestTab(data.results);
+//       console.log(data);
+//     };
+//     call();
+//   }, []);
+
+//   useEffect(() => {
+//     let max = Math.max(score, hightScore);
+//     setHightScore(max);
+//     localStorage.setItem("hightScore", JSON.stringify(max));
+//   }, [score]);
 
   return (
     <div>
--- src/components/question-box.jsx
+++ src/components/question-box.jsx
@@ -21,18 +21,31 @@ export function QuestionBox({
   const [tmpTime, setTmpTime] = useState(10);
   const [questId, setQuestId] = useState(0);
 
+  // useEffect(() => {
+  //   if (!questTab[questId] || heartNumber === 0) return;
+  //   if (tmpTime === 0) {
+  //     handleClick(true, false);
+  //     return;
+  //   }
+  //   const timeout = setInterval(() => {
+  //     setTmpTime((tmpTime) => tmpTime - 1);
+  //   }, 1000);
+
+  //   return () => clearInterval(timeout);
+  // }, [questTab, tmpTime]);
+
   useEffect(() => {
     if (!questTab[questId] || heartNumber === 0) return;
-    if (tmpTime === 0) {
+    if (time === 0) {
       handleClick(true, false);
       return;
     }
     const timeout = setInterval(() => {
-      setTmpTime((tmpTime) => tmpTime - 1);
+      setTime((prev) => prev - 1);
     }, 1000);
 
     return () => clearInterval(timeout);
-  }, [questTab, tmpTime]);
+  }, [questTab, time]);
 
   const handleClick = (value, correct_answer) => {
     if (value === correct_answer) {
@@ -42,7 +55,8 @@ export function QuestionBox({
     }
     setQuestId((prev) => prev + 1);
     if (questId < questTab.length) {
-      setTmpTime(10);
+      setTime(10);
+      // setTmpTime(10);
     }
   };
 
@@ -54,74 +68,72 @@ export function QuestionBox({
           fontSize: "2rem",
         }}
       >
-        {tmpTime <= 0 ? "0" : tmpTime}
+        {/* {tmpTime <= 0 ? "0" : tmpTime} */}
       </p>
       <div className="quest-box">
         <div className="quest-box-main">
           <div className="quest-box-header">Question Box</div>
-          {
-            questTab &&
-            questTab.length > 0 &&
-            questTab[questId] &&
-            heartNumber > 0 ? (
-              <section className="quest-item">
-                <p>{he.decode(questTab[questId].question)}</p>
-                <div className="action-area">
-                  <div>Action Area</div>
-                  <div className="btns">
-                    <button
-                      onClick={() =>
-                        handleClick("True", questTab[questId].correct_answer)
-                      }
-                    >
-                      True
-                    </button>
+          {questTab &&
+          questTab.length > 0 &&
+          questTab[questId] &&
+          heartNumber > 0 ? (
+            <section className="quest-item">
+              <p>{he.decode(questTab[questId].question)}</p>
+              <div className="action-area">
+                <div>Action Area</div>
+                <div className="btns">
+                  <button
+                    onClick={() =>
+                      handleClick("True", questTab[questId].correct_answer)
+                    }
+                  >
+                    True
+                  </button>
 
-                    <button
-                      onClick={() =>
-                        handleClick("False", questTab[questId].correct_answer)
-                      }
-                    >
-                      False
-                    </button>
-                  </div>
+                  <button
+                    onClick={() =>
+                      handleClick("False", questTab[questId].correct_answer)
+                    }
+                  >
+                    False
+                  </button>
                 </div>
-              </section>
-            ) : (!questTab[questId] && questTab.length === 0) ? (
-              <div className="error">
-                <div className="msg-icon alert">
-                  <FiWifiOff />
-                </div>
-                Erreur de connexion, veuillez reessayer
-                <button onClick={() => window.location.reload()}>
-                  <FiRefreshCcw />
-                  Reessayer
-                </button>
               </div>
-            ) : heartNumber <= 0 ? (
-              <div className="error">
-                <div className="msg-icon alert">
-                  <FiFrown />
-                </div>
-                Fin de la partie, Vous avez perdu
-                <button onClick={() => window.location.reload()}>
-                  <FiRotateCcw />
-                  recommencer
-                </button>
+            </section>
+          ) : !questTab[questId] && questTab.length === 0 ? (
+            <div className="error">
+              <div className="msg-icon alert">
+                <FiWifiOff />
               </div>
-            ) : (
-              <div className="error">
-                <div className="msg-icon">
-                  <FiAward />
-                </div>
-                Felicitations, vous avez gagne
-                <button onClick={() => window.location.reload()}>
-                  <FiPlayCircle />
-                  Jouer a nouveau
-                </button>
+              Erreur de connexion, veuillez reessayer
+              <button onClick={() => window.location.reload()}>
+                <FiRefreshCcw />
+                Reessayer
+              </button>
+            </div>
+          ) : heartNumber <= 0 ? (
+            <div className="error">
+              <div className="msg-icon alert">
+                <FiFrown />
               </div>
-            )
-          }
+              Fin de la partie, Vous avez perdu
+              <button onClick={() => window.location.reload()}>
+                <FiRotateCcw />
+                recommencer
+              </button>
+            </div>
+          ) : (
+            <div className="error">
+              <div className="msg-icon">
+                <FiAward />
+              </div>
+              Felicitations, vous avez gagne
+              <button onClick={() => window.location.reload()}>
+                <FiPlayCircle />
+                Jouer a nouveau
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </section>
